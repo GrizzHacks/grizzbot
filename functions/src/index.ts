83e1f6239c65ec642f1fd8bace2e8e4cb8b404a9
Harrison Lavins <harrisonlavins@oakland.edu>
2020-09-14 23:52:52 -0400

    Interactive messages route, Slack JSON parsing; WIP: FB logic, post messages, button handling

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,12 +6,7 @@ import { WebClient } from '@slack/web-api'
 // Initialize Firebase
 admin.initializeApp()
 
-// Initialize Google PubSub
-// const { PubSub } = require('@google-cloud/pubsub')
-// const pubsubClient = new PubSub()
-
 // Initialize Slack bot
-// const SLACK_SIGNING_SECRET = functions.config().slack.signing_secret
 const SLACK_BOT_TOKEN = functions.config().slack.bot_token
 const bot = new WebClient(SLACK_BOT_TOKEN)
 
@@ -68,6 +63,19 @@ function publishMessage(response: functions.Response<any>, id: string, text: str
 
 // ============ Google Cloud Functions ================
 
+// Cloud Function: buttonClickHandler
+// --------------------------
+// Any interactions with shortcuts, modals, or interactive components 
+// (such as buttons, select menus, and datepickers) will be sent to this route
+export const buttonClickHandler = functions.https.onRequest((request, response) => {
+  functions.logger.info("Recieved request to buttonClickHandler", {structuredData: true})
+  const data = request.body
+  console.log(data)
+
+ //send response
+ response.status(200)
+})
+
 // Cloud Function: Hello World - send a message to Slack channel
 // --------------------------
 export const helloWorldMessage = functions.https.onRequest((request, response) => {
@@ -81,54 +89,88 @@ export const helloWorldMessage = functions.https.onRequest((request, response) =
 
 // Cloud Function: getLink
 // --------------------------
-export const getZoomLink = functions.https.onRequest((request, response) => {
-    admin.firestore().doc('queue/quicken').get()
-    .then(snapshot => {
-        const data = snapshot.data()
-        response.send(data) //send contents of the doc as JSON
-    })
-    .catch(error => {
-        //Handle error
-        console.log(error)
-        response.status(500).send(error)
-    })
+export const getDynatraceTest = functions.https.onRequest(async (request, response) => {
+  const channelID = 'sponsor_dynatrace'
+  
+  try {
+    const snapshot = await admin.firestore().doc(`queue/${channelID}`).get()
+    const data = snapshot.data()
+    console.log(data)
+    response.send(data) //send contents of the doc as JSON
+
+  } catch (error) {
+     //Handle error
+     console.log(error)
+     response.status(500).send(error)
+  }
+  
 })
 
 
 // Cloud Function: joinQueue
 // --------------------------
-export const slashJoinQueue = functions.https.onRequest((request, response) => {
+export const slashJoinQueue = functions.https.onRequest(async (request, response) => {
   functions.logger.info("Recieved slash command: joinqueue", {structuredData: true})
-  const data = JSON.stringify(request.body)
+  // Parse and save the incoming Slack data
+  const data = request.body
   console.log(data)
 
- //send response as JSON
-  const responseMessage:string = `Recieved /joinqueue command with request object: '${data}}'`
+  const channelID = data.channel_id
+  const channelName = data.channel_name //use to retrieve sponsor Firebase doc
+  const userID = data.user_id
+  const userName = data.user_name
+  const command = data.command
+
+//   try {
+//     const snapshot = await admin.firestore().doc(`queue/${channelID}`).get()
+//     const data = snapshot.data()
+//     console.log(data)
+//     response.send(data) //send contents of the doc as JSON
+
+//   } catch (error) {
+//      //Handle error
+//      console.log(error)
+//      response.status(500).send(error)
+//   }
+
+
+//   if (!array || !array.length) {
+//     // array or array.length are falsy
+//     // ⇒ do not attempt to process array
+// }
+
+ //send response to Slack as JSON
+  const responseMessage:string = `Recieved /joinqueue command with Slack data:\n 
+  channelID: ${channelID}\n
+  channelName: ${channelName}\n
+  userID: ${userID}\n
+  userName: ${userName}\n
+  command: ${command}\n`
   response.send(responseMessage)
 })
+
 
 // Cloud Function: leaveQueue
 // --------------------------
 export const slashLeaveQueue = functions.https.onRequest((request, response) => {
   functions.logger.info("Recieved slash command: leavequeue", {structuredData: true})
-  const data = JSON.stringify(request.body)
+  const data = request.body
   console.log(data)
 
-   //send response as JSON
-  const responseMessage:string = `Recieved /leavequeue command with request object: '${data}'`
+   //send response to Slack
+   const responseMessage:string = `Recieved /leavequeue command with data: '${JSON.stringify(data)}'`
   response.send(responseMessage)
-
 })
 
 // Cloud Function: advanceQueue
 // --------------------------
 export const slashAdvanceQueue = functions.https.onRequest((request, response) => {
   functions.logger.info("Recieved slash command: advancequeue", {structuredData: true})
-  const data = JSON.stringify(request.body)
+  const data = request.body
   console.log(data)
 
  //send response as JSON
- const responseMessage:string = `Recieved /advancequeue command with args: '${data}'`
+ const responseMessage:string = `Recieved /advancequeue command with args: '${JSON.stringify(data)}'`
  response.send(responseMessage)
 
 })
@@ -138,11 +180,11 @@ export const slashAdvanceQueue = functions.https.onRequest((request, response) =
 // --------------------------
 export const slashShowQueue = functions.https.onRequest((request, response) => {
   functions.logger.info("Recieved slash command: advancequeue", {structuredData: true})
-  const data = JSON.stringify(request.body)
+  const data = request.body
   console.log(data)
 
  //send response as JSON
- const responseMessage:string = `Recieved /showqueue command with args: '${data}'`
+ const responseMessage:string = `Recieved /showqueue command with args: '${JSON.stringify(data)}'`
  response.send(responseMessage)
 
 })
