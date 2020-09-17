703a99cdb1ea8e3d6e3aa503eeb054f2aaaed480
Harrison Lavins <harrisonlavins@oakland.edu>
2020-09-17 01:13:58 -0400

    join, advance, show (updated to display usernames), improved error handling

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -65,7 +65,7 @@ function publishMessage(response: functions.Response<any>, id: string, text: str
 
 // Cloud Function: buttonClickHandler
 // --------------------------
-// Any interactions with shortcuts, modals, or interactive components 
+// Any interactions with Slack shortcuts, modals, or interactive components 
 // (such as buttons, select menus, and datepickers) will be sent to this route
 export const buttonClickHandler = functions.https.onRequest((request, response) => {
   functions.logger.info("Recieved request to buttonClickHandler", {structuredData: true})
@@ -87,24 +87,6 @@ export const helloWorldMessage = functions.https.onRequest((request, response) =
   publishMessage(response, channelID, "Hello world :tada:") 
 })
 
-// Cloud Function: getLink
-// --------------------------
-export const getDynatraceTest = functions.https.onRequest(async (request, response) => {
-  const channelID = 'sponsor_dynatrace'
-  
-  try {
-    const snapshot = await admin.firestore().doc(`queue/${channelID}`).get()
-    const data = snapshot.data()
-    console.log(data)
-    response.send(data) //send contents of the doc as JSON
-
-  } catch (error) {
-     //Handle error
-     console.log(error)
-     response.status(500).send(error)
-  }
-  
-})
 
 
 // Cloud Function: joinQueue
@@ -121,48 +103,59 @@ export const slashJoinQueue = functions.https.onRequest(async (request, response
     const userID = slackData.user_id
     const userName = slackData.user_name
     const command = slackData.command
+    const userMap = {userID, userName} //store each user as a map with their ID and username
 
     //Retrieve Firebase data for this channel
     const snapshot = await admin.firestore().doc(`queue/${channelName}`).get()
     const fbData = snapshot.data()
     console.log(fbData)
 
-    //Construct users array
+    //Get copy of Firebase users array
     const userArray = fbData?.users
-    if (userArray.includes(userID)) {
 
-        //send response to Slack as JSON
-        const responseMessage:string = `Please be patient! You are already in the queue!\n
-        Recieved /joinqueue command with Slack data:\n 
-        channelID: ${channelID}\n
-        channelName: ${channelName}\n
-        userID: ${userID}\n
-        userName: ${userName}\n
-        command: ${command}\n
-        Firebase data:\n
-        ${JSON.stringify(fbData)}`
-        response.send(responseMessage)
-    } else {
+    //If the user is already in the queue, don't add them again
+    let foundDuplicate:boolean = false
+    userArray.forEach((currUser:any) => {
+      if (currUser.userID === userMap.userID){
+        foundDuplicate = true
+      }
+    })
+
+    if (foundDuplicate === false) {
+        //If the user is not already present, continue and add user to Firebase
         try {
-          //add new userID to the queue
-          userArray.push(userID)
-          //update users array in Firebase
+          //add new user to the end of user array and update Firebase
+          userArray.push(userMap)
           await admin.firestore().doc(`queue/${channelName}`).update({
             users: userArray
           })
-          //Respond to Slack
-          response.send(`You have been successfully added to the queue!\n ${userArray}`)
-            
+
+          response.send(`You have been successfully added to the queue! 🐻\n ${userArray}`)
         } catch (error) {
           //Handle error
           console.log(error)
-          response.status(500).send(error)
+          response.status(200).send("GrizzBot is having trouble adding you to the queue - please try again 🐻")
         }
+      
+    } else {
+       //send response to Slack as JSON
+       const responseMessage:string = `You are already in the queue! 🐻\n
+       Recieved /joinqueue command with Slack data:\n 
+       channelID: ${channelID}\n
+       channelName: ${channelName}\n
+       userID: ${userID}\n
+       userName: ${userName}\n
+       command: ${command}\n
+       Firebase data:\n
+       ${JSON.stringify(fbData)}`
+       response.send(responseMessage)
     }
+
+
   } catch (error) {
      //Handle error
      console.log(error)
-     response.status(500).send(error)
+     response.status(200).send("GrizzBot is not trained to work in this channel 🐻")
   }
 
 })
@@ -193,7 +186,7 @@ export const slashAdvanceQueue = functions.https.onRequest(async (request, respo
 
       // if the queue is empty or null, do not attempt to process
       if (!userArray || !userArray.length) {
-            response.send(`advancequeue: The ${channelName} queue is empty!`)
+            response.send(`advancequeue: The ${channelName} queue is empty! 🐻`)
         }
 
       //Advance the queue and update the Firebase users array
@@ -202,21 +195,60 @@ export const slashAdvanceQueue = functions.https.onRequest(async (request, respo
         users: userArray
       })
 
-      response.send(`advancequeue: Successfully advanced the ${channelName} queue!\nA new user will join the call soon! `)
+      response.send(`advancequeue: Successfully advanced the ${channelName} queue!\nA new user will join the call soon! 🐻`)
     } else {
-      response.send("advancequeue: Invalid sponsor key, try again!")
+      response.send("advancequeue: Invalid sponsor key or wrong channel, try again! 🐻")
     }
 
     
   } catch (error) {
      //Handle error
      console.log(error)
-     response.status(500).send(error)
+     response.status(200).send("GrizzBot is not trained to work in this channel 🐻")
   }
 
 })
 
+// Cloud Function: showQueue
+// --------------------------
+export const slashShowQueue = functions.https.onRequest(async (request, response) => {
+  try {
+      functions.logger.info("Recieved slash command: showqueue", {structuredData: true})
+      // Parse and save the incoming Slack data
+      const slackData = request.body
+      console.log(slackData)
+    
+      const channelName = slackData.channel_name //use to retrieve sponsor Firebase doc
+      // const userID = slackData.user_id        //use to send ephemeral message back to user
 
+      //Retrieve Firebase data for this channel
+      const snapshot = await admin.firestore().doc(`queue/${channelName}`).get()
+      const fbData = snapshot.data()
+      console.log(fbData)
+
+      //Construct users array
+      const userArray = fbData?.users
+      // if the queue is empty or null, do not attempt to process and return
+      if (!userArray || !userArray.length) {
+        response.send(`showqueue: The ${channelName} queue is empty! 🐻`)
+      }
+
+      //Construct queue status string and send response to Slack
+      let responseMessage:string = `The current queue for ${channelName}:\n`
+      let i = 1
+      userArray.forEach((currUser:any) => {
+        responseMessage += `${i}. ${currUser.userName}\n`
+        i++
+      })
+      response.send(responseMessage)
+  
+  } catch (error) {
+     //Handle error
+     console.log(error)
+     response.status(200).send("GrizzBot is not trained to work in this channel 🐻")
+  }
+
+})
 
 
 // Cloud Function: leaveQueue
@@ -231,15 +263,3 @@ export const slashLeaveQueue = functions.https.onRequest((request, response) => 
   response.send(responseMessage)
 })
 
-// Cloud Function: showQueue
-// --------------------------
-export const slashShowQueue = functions.https.onRequest((request, response) => {
-  functions.logger.info("Recieved slash command: showQueue", {structuredData: true})
-  const data = request.body
-  console.log(data)
-
- //send response as JSON
- const responseMessage:string = `Recieved /showqueue command with args: '${JSON.stringify(data)}'`
- response.send(responseMessage)
-
-})
